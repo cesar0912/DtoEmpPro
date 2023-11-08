8bba0a15fd1257f19d4711d11d14f9eafc8b0845
M <M@host.docker.internal>
2023-11-08 12:41:06 +0100

    Archivos sql

---
+++ DtoEmpPro2/src/main/resources/META-INF/sql/create.sql
@@ -0,0 +1,2 @@
+CREATE TABLE hib_departamento (id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, nombre varchar(50) NOT NULL UNIQUE);
+CREATE TABLE hib_empleado (id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, nombre varchar(100) NOT NULL, salario decimal(10,2) DEFAULT NULL, departamento int(11) DEFAULT NULL, FOREIGN KEY (departamento) REFERENCES hib_departamento(id) ON DELETE SET NULL ON UPDATE CASCADE);
---
+++ DtoEmpPro2/src/main/resources/META-INF/sql/drop.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS hib_empleado;
+DROP TABLE IF EXISTS hib_departamento;
